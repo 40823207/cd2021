7392755977d72ec36bd798cecbe091fdfeaf6dde
40823207 <40823207@gm.nfu.edu.tw>
2021-03-09 20:00:43 +0800

    leo

--- publishconf.py
+++ publishconf.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
 
@@ -13,7 +14,7 @@ from pelicanconf import *
 # 因為 publishconf.py 在 pelicanconf.py 之後, 因此若兩處有相同變數的設定, 將以較後讀入的 publishconf.py 中的設定為主.
 
 # 將所有靜態 html 檔案移到 blog 子目錄
-SITEURL = 'https://chiamingyen.github.io/cmsimfly/blog'
+SITEURL = 'https://40823207.github.io/cd2021/blog'
 # 此設定用於將資料送到 gh-pages, 因此使用絕對 URL 設定
 RELATIVE_URLS = False
 
@@ -30,7 +31,7 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-DISQUS_SITENAME = "kmolab-github"
+DISQUS_SITENAME = "40823207-cd2021"
 DISQUS_DISPLAY_COUNTS = False
 #GOOGLE_ANALYTICS = ""
 
